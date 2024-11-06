6965ea071bc5e7d2aa38e508efae7b1ebaac88cf
leokozan <113151772+leokozan@users.noreply.github.com>
2024-11-06 09:43:25 -0300

    tela de login

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,62 +1,90 @@
-import React, { useState ,FormEvent} from 'react';
-import { TextField, Button, Typography, Container, Box } from '@mui/material';
+import React, { useState } from 'react';
+import { TextField, Button, Typography, Box, Alert } from '@mui/material';
+import { User } from '../models/models';
 import Grid from '@mui/material/Grid2';
+
 function LoginPage() {
-    const [email, setEmail] = useState<string>('');
-    const [password, setPassword] = useState<string>('');
-    const [error, setError] = useState<string>('');
-  
-    // Função de login
-    const handleLogin = (e: FormEvent<HTMLFormElement>): void => {
-        console.log('Login feito');
-    };
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+  const [error, setError] = useState<string>(''); // Estado para armazenar a mensagem de erro
+
+  const users: User[] = [
+    {
+      email: 'medico@farma.com',
+      password: 'admin123',
+      cargo: 'medico',
+    },
+    {
+      email: 'farmaceutico@farma.com',
+      password: 'func123',
+      cargo: 'farmaceutico',
+    },
+  ];
+
+  const handleLogin = () => {
+    if (!email || !password) {
+      setError('Por favor, preencha todos os campos.');
+      return;
+    }
+    const user = users.find((u) => u.email === email && u.password === password);
+    if (user) {
+      alert(`Login bem-sucedido! Cargo: ${user.cargo}`);
+      setError(''); // Limpa qualquer erro anterior ao fazer login com sucesso
+    } else {
+      setError('Usuário ou senha inválidos.');
+    }
+  };
 
   return (
     <Grid container>
-        <Grid size={{xs:12}}>
-            <Box padding={4} boxShadow={1} display={'flex'} flexDirection={'column'} alignItems={'center'}>
-                <Typography variant="h5" component="h1" sx={{ marginBottom: 3 }}>
-                Login - Farma
-                </Typography>
-                <Box>
-                <TextField
-                    label="Email"
-                    type="email"
-                    variant="outlined"
-                    fullWidth
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                    margin="normal"
-                    required
-                />
-                <TextField
-                    label="Senha"
-                    type="password"
-                    variant="outlined"
-                    fullWidth
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                    margin="normal"
-                    required
-                />
-                
-                <Button
-                    type="submit"
-                    variant="contained"
-                    fullWidth
-                    sx={{ marginTop: 2 }}
-                    color="primary"
-                >
-                    Entrar
-                </Button>
-                </Box>
-                <Grid size={{xs:12}}>
-                    <Typography variant="body2">
-                    Esqueceu a senha?
-                    </Typography>
-                </Grid>
+      <Grid  size={12}>
+        <Box padding={4} boxShadow={1} display={'flex'} flexDirection={'column'} alignItems={'center'}>
+            <Typography variant="h5" component="h1" sx={{ marginBottom: 3 }}>
+            Login - Farma
+            </Typography>
+            <Box>
+            {error && (
+                <Alert severity="error" sx={{ marginTop: 2}}>
+                {error}
+                </Alert>
+            )}
+            <TextField
+                label="Email"
+                type="email"
+                variant="outlined"
+                fullWidth
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                margin="normal"
+                required
+            />
+            <TextField
+                label="Senha"
+                type="password"
+                variant="outlined"
+                fullWidth
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                margin="normal"
+                required
+            />
+            <Button
+                variant="contained"
+                fullWidth
+                sx={{ marginTop: 2 }}
+                color="primary"
+                onClick={handleLogin}
+            >
+                Entrar
+            </Button>
             </Box>
-        </Grid>
+            <Grid size={12}>
+            <Typography variant="body2">
+                Esqueceu a senha?
+            </Typography>
+            </Grid>
+        </Box>
+      </Grid>
     </Grid>
   );
 }
